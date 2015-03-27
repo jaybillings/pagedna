20592aa2732c7e50815c53b803be59bfca00dbc9
Jay Billings <contact@scribbleowl.com>
2015-03-26 19:38:50 -0700

    Renders color blocks from JSON

--- js/main.js
+++ js/main.js
@@ -1,31 +1,95 @@
-//
-// To-Do example
-// You can replace this file with any of the examples from agilityjs.com...
-// ... they work right out of the box!
-//
-
-//
-// Item prototype
-//
-var item = $$({}, '<li><span data-bind="content"/> <button>x</button></li>', '& span { cursor:pointer; }', {
-  'click span': function(){
-    var input = prompt('Edit to-do item:', this.model.get('content'));
-    if (!input) return;
-    this.model.set({content:input});
-  },
-  'click button': function(){
-    this.destroy();
-  }
+// Color blocks within cell
+var colorBlock = $$({
+    model: {
+        color: 'blue'
+    },
+    view: {
+        format: '<div data-bind="class=color"></div>'
+    },
+    controller: {}
 });
 
-//
-// List of items
-//
-var list = $$({}, '<div> <button id="new">New item</button> <ul></ul> </div>', {
-  'click #new': function(){
-    var newItem = $$(item, {content:'Click to edit'});
-    this.append(newItem, 'ul'); // add to container, appending at <ul>
-  }
+// Table cell
+var cell = $$({}, '<td></td>');
+
+// Table row
+var row = $$({
+    model: {
+        columns: ['A', 'B'],
+        sequence: ["red", "green", "yellow", "blue"],
+        states: {
+            "red": "A",
+            "green": "A",
+            "yellow": "A",
+            "blue": "A"
+        },
+        content: function () {
+            
+        }()
+    },
+    view: {
+       format: '<tr data-bind="content"></tr>'
+    },
+    controller: {
+        'create': function() {
+            var me = this;
+            for (var i = 0; i < me.model.get('columns').length; i++) {
+                var newCell = $$(cell, {});
+                $.each(me.model.get('sequence'), function (j, val) {
+                    if (me.model.get('states')[val] === me.model.get('columns')[i]) {
+                        var newColorBlock = $$(colorBlock, {color: val});
+                        newCell.append(newColorBlock);
+                    }
+                })
+                me.append(newCell);
+            }
+            $$.document.append(me, 'table');
+        },
+        'click td': function (e) {
+            console.log(this.model.get('states'));
+            console.log(this.model.size());
+            var me = this,
+                color = e.target.className,
+                states = me.model.get('states'),
+                colName = states[color],
+                colNum = me.model.get('columns').indexOf(colName),
+                newColNum = colNum + 1 < me.model.get('columns').length ? colNum + 1 : 0;
+            states[color] = me.model.get('columns')[newColNum];
+            me.model.set({'states': states});
+            me.view.render();
+        }
+    }
 });
 
-$$.document.append(list);
+// Table
+
+var table = $$({
+    model: {
+        columns: ['A', 'B']
+    },
+    view: {
+        format:
+        '<table id="dna-table">\
+            <tr>\
+                <th>A</th><th>B</th>\
+            </tr>\
+        </table>'
+    },
+    controller: {
+        'create': function () {
+            var me = this;
+            $.getJSON("data/rows.json", function (data) {
+                for (var i in data) {
+                    var newRow = $$(row, {sequence: data[i].sequence, states: data[i].states});
+                    me.append(newRow);
+                }
+            });
+        }
+    }
+});
+$$.document.append(table);
+
+
+// Add column form
+
+// Get JSON form
