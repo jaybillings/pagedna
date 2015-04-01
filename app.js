e0991f2d4c983c07507d1832bde780b915713569
Jay Billings <contact@scribbleowl.com>
2015-03-31 18:23:08 -0700

    Wraps app in node for deploying + style tweaks

---
+++ app.js
@@ -0,0 +1,8 @@
+/* Retrieved from https://github.com/expressjs/serve-static */
+var express = require('express');
+var serveStatic = require('serve-static');
+
+var app = express();
+
+app.use(serveStatic('./public', {'index': ['index.html', 'index.htm']}));
+app.listen(3000);
